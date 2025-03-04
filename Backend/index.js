810194b0a6bac5c178aa0223d710ef8943ac970b
unknown <ajaykarthikeyan436@gmail.com>
2025-03-04 16:39:15 +0530

    Created Bulkmail-App

---
+++ Backend/index.js
@@ -0,0 +1,83 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require('mongoose');
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect("mongodb+srv://ajaykarthikeyan436:1234@cluster0.lj0sn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(function(){console.log("Connected To DB")})
+.catch(function(){console.log("Failed To Connect")})
+
+const credential = mongoose.model("credential",{},"bulkmail")
+
+
+app.post("/sendemail", function (req, res) {
+
+    console.log("Received Data:", req.body);
+
+    var msg = req.body.msg
+    var emailList = req.body.emaillist
+
+    if (!emailList || !Array.isArray(emailList)) {
+        return res.status(400).json({ error: "Invalid email list" });
+    }
+
+    console.log(emailList,msg)
+
+    credential.find().then(function(data){
+
+        console.log(data[0].toJSON().user)
+    
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: data[0].toJSON().user,
+                pass: data[0].toJSON().pass,
+            },
+        });
+    
+        new Promise(async function (resolve, reject) {
+            try {
+                for (var i = 0; i <= emailList.length; i++) 
+                    {
+                    await transporter.sendMail(
+                        {
+                            from: "ajaykarthikeyan436@gmail.com",
+                            to: emailList[i],
+                            subject: "A Messsage From Bulkmail App",
+                            text: msg
+                        },
+                    )
+    
+                    console.log("Email sent to:"+emailList[i])
+                }
+    
+                resolve("Success")
+            }
+            catch (error) {
+                reject("Failed")
+            }
+        }).then(function(){
+            res.send(true)
+        }).catch(function(){
+            res.send(false)
+        })
+    
+    }).catch(function(error){
+        console.log(error)
+    })
+})
+
+const nodemailer = require("nodemailer");
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
+
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react"
+import axios from "axios"
+import * as XLSX from "xlsx"
+
+function App() {
+
+  const [msg, setmsg] = useState("")
+  const [status, setStatus] = useState(false)
+  const [emailList, setEmailList] = useState([])
+
+  function handlemsg(evt) {
+    setmsg(evt.target.value)
+  }
+
+  function handlefile(event) {
+    const file = event.target.files[0]
+    console.log(file)
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+
+      const emailList = XLSX.utils.sheet_to_json(worksheet, { header: "A" });
+
+      const totalemail = emailList.map(function (item) {
+        return item.A;
+      });
+
+      console.log("Extracted Emails:", totalemail);
+      setEmailList(totalemail);
+    };
+
+    reader.readAsArrayBuffer(file);
+  }
+
+  function send() {
+    console.log("Sending Emails:", emailList);
+    setStatus(true);
+
+    axios
+        .post("https://backend-mkbudwytx-ajays-projects-8ce49744.vercel.app/sendemail", { msg: msg, emaillist: emailList })
+        .then(function (data) {
+            if (data.data === true) {
+                alert("Email Sent Failed");
+                setStatus(false)
+            } else {
+                alert("Email Sent Successfully");
+                setStatus(false)
+            }
+        })
+        .catch(function (error) {
+            console.error("Error in sending email:", error);
+        });
+}
+
+
+  return (
+    <div>
+      <div className="bg-blue-950 text-white text-center">
+        <h1 className="text-2xl font-medium px-5 py-3">BulkMail</h1>
+      </div>
+
+      <div className="bg-blue-800 text-white text-center">
+        <h1 className="font-medium px-5 py-3">We can help your bussiness with sending multiple emails at once</h1>
+      </div>
+
+      <div className="bg-blue-600 text-white text-center">
+        <h1 className="font-medium px-5 py-3">Drag and Drop</h1>
+      </div>
+
+      <div className="bg-blue-400 flex flex-col items-center text-black px-5 py-3">
+        <textarea onChange={handlemsg} value={msg} className="w-[80%] h-32 py-2 px-2 outline-none border border-black rounded-md" placeholder="Enter the Email text..."></textarea>
+
+        <div>
+          <input onChange={handlefile} type="file" className="border-4 border-dashed py-4 px-4 mt-5 mb-5"></input>
+        </div>
+
+        <p>Total Emails in the File: {emailList.length}</p>
+
+        <button onClick={send} className="bg-blue-950 py-2 px-2 text-white font-medium rounded-md w-fit">{status ? "Sending..." : "Send"}</button>
+
+      </div>
+
+      <div className="bg-blue-300 text-white text-center p-8">
+
+      </div>
+
+      <div className="bg-blue-200 text-white text-center p-8">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
